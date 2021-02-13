ee1cbbf830a94defba04cba529d4ed7ab70d9dc2
pakkun <pakkun_kh@fol.hi-ho.ne.jp>
2021-02-13 13:44:57 +0900

    Fixed indent

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -1,17 +1,17 @@
-//
-//  LandmarksApp.swift
-//  Landmarks
-//
-//  Created by pakkun on 2021/02/13.
-//
-
-import SwiftUI
-
-@main
-struct LandmarksApp: App {
+  //
+  //  LandmarksApp.swift
+  //  Landmarks
+  //
+  //  Created by pakkun on 2021/02/13.
+  //
+  
+  import SwiftUI
+  
+  @main
+  struct LandmarksApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+      WindowGroup {
+        ContentView()
+      }
     }
-}
+  }
