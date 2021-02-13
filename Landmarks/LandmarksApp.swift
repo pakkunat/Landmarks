2ab3c0f8553a1b645b47bf9d550090895be77fbe
pakkun <pakkun_kh@fol.hi-ho.ne.jp>
2021-02-13 13:43:54 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by pakkun on 2021/02/13.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
