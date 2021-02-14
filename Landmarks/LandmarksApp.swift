cc2a6927629344786a010997229f54f38618ccb6
pakkun <pakkun_kh@fol.hi-ho.ne.jp>
2021-02-14 11:32:33 +0900

    Handling User Input Section 5 Created.

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -9,9 +9,12 @@
   
   @main
   struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
       WindowGroup {
         ContentView()
+          .environmentObject(modelData)
       }
     }
   }
